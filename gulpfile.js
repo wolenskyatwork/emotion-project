fa217e6c8302812c3fac4b88dfdbc0dd2758bd96
wolenskyatwork <liz.wolensky@gmail.com>
2017-02-02 16:34:54 -0500

    ESLint added

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,7 @@ var browserify = require('browserify');
 var reactify = require('reactify'); //jsx to js
 var source = require('vinyl-source-stream'); //conventional text streams
 var concat = require('gulp-concat');
+var lint = require('gulp-eslint');
 
 var  config = {
   port: '9005',
@@ -60,9 +61,15 @@ gulp.task('css', function() {
     .pipe(gulp.dest(config.paths.dist + '/css'));
 });
 
-gulp.task('watch', function() {
-  gulp.watch(config.paths.html, ['html']);
-  gulp.watch(config.paths.js, ['js']);
+gulp.task('lint', function() {
+  return gulp.src(config.paths.js)
+    .pipe(lint({ configFile: '.eslintrc.json' }))
+    .pipe(lint.format());
 });
 
-gulp.task('default', ['html', 'js', 'css', 'open', 'watch']);
+gulp.task('watch', function() {
+  gulp.watch(config.paths.html, ['html']);
+  gulp.watch(config.paths.js, ['js', 'lint']);
+});
+
+gulp.task('default', ['html', 'js', 'css', 'lint', 'open', 'watch']);
